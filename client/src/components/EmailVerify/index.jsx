6b1fe565efe1bcd0da346907c763ffe3c142ad48
Zian Shang <zian.shang@stonybrook.edu>
2023-10-28 22:53:18 -0400

    add email verify component

---
+++ client/src/components/EmailVerify/index.jsx
@@ -0,0 +1,42 @@
+import {useState, useEffect, Fragment} from 'react';
+import {Link, useParams} from 'react-router-dom';
+import success from '../../images/success.png';
+import styles from "./styles.module.css";
+
+const EmailVerify =()=>{
+    const [validUrl, setValidUrl] = useState(false);
+    const param = useParams();
+
+    useEffect(()=>{
+        const verifyEmailUrl = async ()=>{
+            try{
+                const url = `http://localhost:8080/api/users/${param.id}/verify/${param.token}`;
+                const{data} =await axios.get(url);
+                console.log(data);
+                setValidUrl(true);
+            }catch(error){
+                console.log(error);
+                setValidUrl(false);
+            }
+        };
+        verifyEmailUrl()
+    },[param])
+
+    return(
+        <Fragment>
+            {validUrl?(
+                <div className={styles.container}>
+                    <img src={success} alt='success_img' className={styles.success.img}></img>
+                    <h1>Email verified successfully</h1>
+                    <Link to = "/login">
+                        <button className={styles.green_btn}>Login</button>
+                    </Link>
+                </div>
+            ):(
+                <h1>404 Not Found</h1>
+            )}
+        </Fragment>
+    )
+};
+
+export default EmailVerify;
